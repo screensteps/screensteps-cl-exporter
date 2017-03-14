c06c5b94dd38a6ab251188b96a2f9e8d4ed1f27c
Mike Karp <mdkarp@gmail.com>
2017-03-14 18:38:33 -0400

    basic pulling data works

--- run.py
+++ run.py
@@ -6,18 +6,18 @@ import json
 
 def main(argv):
     # Define variables we need.
-    site_name = 'customerknowledge'
+    site_name = 'customerknowledge' #n / name
     user_id = 'mkarp' #u / user
     api_token = 'silly' #p / password
     template_folder = '' #t / template
     output_folder = '' #o / output
-    site_id = '14885' #s / site
-    manual_id = '61817' #m / manual
-    article_id = '' #a / article
+    site_id = '15226' #s / site
+    manual_id = '58254' #m / manual
+    article_id = '612809' #a / article
     try:
-        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+        opts, args = getopt.getopt(argv,"hn:u:p:t:o:s:m:a:",["name=","user=","password=","template_folder=","output_folder=","site_id=","manual_id=","article_id="])
     except getopt.GetoptError:
-        print 'test.py -i <inputfile> -o <outputfile>'
+        print 'run.py -i <inputfile> -o <outputfile>'
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
@@ -30,8 +30,8 @@ def main(argv):
     #print 'Input file is "', inputfile
     #print 'Output file is "', outputfile
 
-    # set up URLs
-    def ss_request(endpoint):
+    # set up request
+    def screensteps(endpoint):
         base_url = 'https://' + site_name + '.screenstepslive.com/api/v2/'
         site_endpoint = base_url + endpoint
         try:
@@ -45,9 +45,48 @@ def main(argv):
             print 'Error connecting to server (' + str(r.status_code) + ')'
             sys.exit(2)
 
-    print("Connected to Sites")
-    t = ss_request('sites')
-    print(t)
+    # grab all sites for that user information
+    print("> Pulling sites")
+    sites = screensteps('sites') # grab sites
+    print("> " + str(sites))
+
+    # loop through sites
+    for site in sites['sites']:
+        this_site_id = str(site['id'])
+        if (site_id == this_site_id) or (site_id == ''): # only action a site if site_id isn't set, or is a match
+            print(">> Processing site: " + site['title'])
+            print(">> " + str(site))
+            manuals = screensteps('sites/' + this_site_id) #grab manuals
+
+            # loop through manuals
+            for manual in manuals['site']['manuals']:
+                this_manual_id = str(manual['id'])
+                if (manual_id == this_manual_id) or (manual_id == ''): # only action a manual if manual isn't set, or is a match
+                    print(">>> Processing manual: " + manual['title'])
+                    print(">>> " + str(manual))
+
+                    chapters = screensteps('sites/' + this_site_id + '/manuals/' + this_manual_id) # grab chapters
+
+                    # loop through chapters
+                    for chapter in chapters['manual']['chapters']:
+                        this_chapter_id = str(chapter['id'])
+                        print(">>>> Processing chapter: " + chapter['title'])
+                        print(">>>> " + str(chapter))
+
+                        articles = screensteps('sites/' + this_site_id + '/chapters/' + this_chapter_id) # grab articles
+
+                        # loop through articles
+                        for article in articles['chapter']['articles']:
+                            this_article_id = str(article['id'])
+                            if (article_id == this_article_id) or (article_id == ''): # only action an article if article_id isn't set, or is a match
+                                print(">>>>> Processing article: " + article['title'])
+                                print(">>>>> " + str(article))
+
+                                this_article = screensteps('sites/' + this_site_id + '/articles/' + this_article_id) # grab ind article
+                                print("-- BEGIN HTML --")
+                                print(this_article['article']['html_body'])
+                                print("-- END HTML --")
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
